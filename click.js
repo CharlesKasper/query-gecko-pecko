19bb6c937171a9fbe140e1f43765040498f1e0d3
CharlesKasper <7ckasper@stpatrick.org>
2017-03-21 10:48:41 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,12 @@
+//declare samecolor() here
+function samecolor(hex){
+    let samecolor = doc.getElementById('samecolor')
+        div.background-color: #529190;
+        for (var i = 0; i < samecolor.length; i++) {
+            div.i opacity= Math.rand
+    }
+}
+//declare diffcolor() here
+function diffcolor(){
+
+}
