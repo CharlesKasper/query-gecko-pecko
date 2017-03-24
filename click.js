843a6a0d7c1b7910d03a0b15765a7acc7f0c9043
CharlesKasper <7ckasper@stpatrick.org>
2017-03-24 10:38:08 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -5,19 +5,23 @@ function samecolor(hex){
     let divs = doc.querySelectorAll("samecolor div");
         for (var i = 0; i < divs.length; i++) {
         divs[i].style.backgroundColor= color;
-        divs[i].opacity=Math.rand;
+        var temp= Math.rand;
+        divs[i].opacity=temp;
     }
 }
 //declare diffcolor() here
 function diffcolor(){
-let color= 743212;
-let complement= 092371;
-let divs = doc.querySelectorAll("diffcolor div");
-    for (var i = 0; i < divs.length; i++) {
-        if i= %2==0{
-        divs[i].style.backgroundColor=color;
-    }
-        else
-        divs[i].style.backgroundColor=complement
+    const doc= document;
+    let color= 743212;
+    let complement= 092371;
+    let divs = doc.querySelectorAll("diffcolor div");
+
+  for(var i=0; i<divs.length; i++){
+    if(i % 2 == 0){
+     divs[i].style.backgroundColor=color;
+ }
+  else{
+    divs[i].style.backgroundColor=complement;
+  }
     }
 }
